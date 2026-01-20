41fd14b0954b7dac58a430a7e8fcb575b327eb0d
StefanoDamato <stefano.damato@idsia.ch>
2026-01-20 10:51:48 +0100

    New project organisation sketch

---
+++ src/main.py
@@ -0,0 +1,36 @@
+# generate for me a python script that given the model, launches global.py with the rigght parameters
+
+import subprocess
+import argparse
+import os
+import sys
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="local vs global models for intermittent time series forecasting")
+    parser.add_argument('--dataset_name', type=str, choices=['OnlineRetail', 'Auto', 'RAF', 'carparts', 'syph', 'M5', 'crime', 'VN1', 'UCI'], required=True, help='Specify dataset name')
+    parser.add_argument('--model', type=str, choices=['deepAR','transformer','informer', 'autoformer', 'feedforward', 'dlinear'], required=True, help="Specify model")
+    parser.add_argument('--distribution_head', type=str, choices=['poisson','negbin', 'tweedie', 'zinb'], default=None, help="Specify distribution_head, default is None")
+    parser_args = parser.parse_args()
+    
+    if parser_args.model in ['deepAR','transformer','informer', 'autoformer', 'feedforward', 'dlinear']:
+        if parser_args.distribution_head is None:
+            raise ValueError("distribution_head must be specified for the selected model.")
+        cmd = [
+            sys.executable,  # Use the current Python interpreter
+            os.path.join('src', 'global.py'),
+            '--dataset_name', parser_args.dataset_name,
+            '--model', parser_args.model,
+            '--distribution_head', parser_args.distribution_head
+        ]
+
+    if parser_args.model in ['isq', 'iETS', 'tweedieGP']:
+        cmd = [
+            sys.executable,  # Use the current Python interpreter
+            os.path.join('src', 'local.py'),
+            '--dataset_name', parser_args.dataset_name,
+            '--model', parser_args.model
+        ]
+        
+        
+        
+    subprocess.run(cmd)
